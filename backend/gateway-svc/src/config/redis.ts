93b667ebc90a0b3f3d79a9c081953565f5897faa
ezelpc <ezequielpc496@gmail.com>
2025-11-30 23:41:18 -0600

    fix: use redis service name in fallback URL

--- backend/gateway-svc/src/config/redis.ts
+++ backend/gateway-svc/src/config/redis.ts
@@ -3,18 +3,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
+const REDIS_URL = process.env.REDIS_URL || 'redis://redis:6379';
 
 export const redisClient = createClient({
   url: REDIS_URL,
-  socket:{
+  socket: {
     reconnectStrategy: (retries: number) => Math.min(retries * 50, 1000)
-    }
+  }
 });
 
 redisClient.on('error', (err: any) => console.log('❌ Redis Client Error', err));
 redisClient.on('connect', () => console.log('✅ Conectado a Redis'));
 
 export const connectRedis = async () => {
-    await redisClient.connect().catch(console.error);
+  await redisClient.connect().catch(console.error);
 }; 
