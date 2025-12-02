71289aa20e7d6ae3f9bcae978fde9e987c942a59
ezelpc <ezequielpc496@gmail.com>
2025-12-01 22:06:44 -0600

    Eliminacion de dist

--- backend/notificaciones-svc/dist/events/chat.events.js
+++
@@ -1,5 +0,0 @@
-import { sendChatNotification } from '../Services/notification.service.js';
-export const handleChatEvent = async (msg) => {
-    const data = JSON.parse(msg.content.toString());
-    await sendChatNotification(data);
-};
--- backend/notificaciones-svc/dist/events/ticket.events.js
+++
@@ -1,5 +0,0 @@
-import { sendTicketNotification } from '../Services/notification.service.js';
-export const handleTicketEvent = async (msg) => {
-    const data = JSON.parse(msg.content.toString());
-    await sendTicketNotification(data);
-};
